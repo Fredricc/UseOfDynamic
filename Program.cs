5c14dc9a1f1bddecbcc6efee4abe7e674ae49fd1
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-02-11 19:39:14 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace UseOfDynamic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic text = "some text";
+
+            var toUpper = text.ToUpper();
+
+            Console.WriteLine(toUpper);
+        }
+    }
+}
