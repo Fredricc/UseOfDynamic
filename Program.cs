23a9d056e635c4ca581c222e22cf39422f1a654c
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-02-12 06:52:00 +0300

    type checking happens at runtime with dynamic type as compared to static type where type checking happens at compile time.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
 ﻿namespace UseOfDynamic
 {
+    public class SomeClass
+    {
+        public string Text { get; }
+
+        public SomeClass(dynamic hopefullyText)
+        {
+            Text = hopefullyText;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            //var someClass = new SomeClass();
+
             dynamic text = "some text";
 
             var toUpper = text.ToUpper();
